59fa7d28b93af0f6290e8502beb613f977233091
21DP2JFern <12djfernats@rvt.lv>
2025-06-02 21:47:34 +0300

    asd

--- backend/app/Http/Controllers/Api/ForumController.php
+++ backend/app/Http/Controllers/Api/ForumController.php
@@ -305,9 +305,10 @@ class ForumController extends Controller
         ]);
 
         $post = Post::findOrFail($postId);
+        $user = auth()->user();
 
-        // Check if the user is the author of the post
-        if ($post->user_id !== auth()->id()) {
+        // Check if the user is either the author of the post or an admin
+        if ($post->user_id !== $user->id && $user->role !== 'admin') {
             return response()->json([
                 'status' => false,
                 'message' => 'You are not authorized to edit this post'
